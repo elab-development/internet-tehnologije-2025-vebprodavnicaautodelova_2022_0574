389fbcd242344b05c3dcdcb99aa3d54078a955d3
IvanaJovanovic0289 <jovanovicivana27012004@gmail.com>
2026-02-01 11:18:33 +0100

    Deveti commit2-Router Init

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,37 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Home from './pages/Home';
+import Products from './pages/Products';
+import ProductDetails from './pages/ProductDetails';
+
+import Login from './pages/Login';
+import Register from './pages/Register';
+
+import Profile from './pages/Profile';
+import Cart from './pages/Cart';
+import Checkout from './pages/Checkout';
+import OrderDetails from './pages/OrderDetails';
+import AdminDashboard from './pages/AdminDashboard';
+
 function App() {
-  return <>PitStopShop</>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/products' element={<Products />} />
+        <Route path='/products/:productId' element={<ProductDetails />} />
+
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+
+        <Route path='/profile' element={<Profile />} />
+        <Route path='/cart' element={<Cart />} />
+        <Route path='/checkout' element={<Checkout />} />
+        <Route path='/order/:orderId' element={<OrderDetails />} />
+        <Route path='/admin' element={<AdminDashboard />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
